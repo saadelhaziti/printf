10bb7ed3a637fbb2c05686afb8ff27400f1286bb
DZEK00 <zakarialyazale7@gmail.com>
2023-04-23 23:22:32 +0100

    task0

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _putchar(char c);
+int print_chare(va_list arg);
+int print_string(va_list arg);
+int _printf(const char *format, ...);
+int print_integer(va_list arg);
+int print_binary(va_list arg);
+int switche(va_list list, char format);
+int print_min_int(void);
+int count_digits(int n);
+
+#endif
