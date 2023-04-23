fc636ae0cf4bd55dc6ee63a6be35beac30dd8316
DZEK00 <zakarialyazale7@gmail.com>
2023-04-23 23:24:34 +0100

    task0

---
+++ search_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * switche -> find the character
+ *
+ * @list: argument
+ * @format: character for switch
+ *
+ * Return: counter
+ */
+
+int switche(va_list list, char format)
+{
+	int count = 0;
+
+	switch (format)
+	{
+		case 'c':
+			count += print_chare(list);
+			break;
+		case 's':
+			count += print_string(list);
+			break;
+		case 'd':
+		case 'i':
+			count += print_integer(list);
+			break;
+		case '%':
+			_putchar('%');
+			count++;
+			break;
+		case 'b':
+			count += print_binary(list);
+			break;
+		default:
+			_putchar('%');
+			_putchar(format);
+			count += 2;
+	}
+
+	return (count);
+}
