e132d66761592ad9d838fb7cc539593100fef01d
DZEK00 <zakarialyazale7@gmail.com>
2023-04-23 23:23:17 +0100

    task0

---
+++ printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _printf -> Prints a formatted string
+ *
+ * @format: Input String Format
+ *
+ * Return: Number chars Printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list list;
+
+	va_start(list, format);
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				count += switche(list, *format);
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(list);
+	return (count);
+}
