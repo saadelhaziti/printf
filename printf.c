e3e1d66e95ff6d610fb603d8d8f1a510f65f5910
DZEK00 <zakarialyazale7@gmail.com>
2023-04-24 17:03:19 +0100

    update

--- printf.c
+++ printf.c
@@ -14,18 +14,20 @@ int _printf(const char *format, ...)
 	va_list list;
 
 	va_start(list, format);
-	if (!format || (format[0] == '%' && !format[1])){
-        return (-1);
-}
-    if (format[0] == '%' && format[1] == ' ' && !format[2]){
-        return (-1);
-		}
+	if (!format || (format[0] == '%' && !format[1]))
+	{
+		return (-1);
+	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
 	while (format && *format)
 	{
 		if (*format == '%')
 		{
 			format++;
-				count += switche(list, *format);
+			count += switche(list, *format);
 		}
 		else
 		{
