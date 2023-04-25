984b5134c6dd4f01c9fe1123c25a6db6cd56724f
DZEK00 <zakarialyazale7@gmail.com>
2023-04-25 01:18:14 +0100

    update print_base

--- str_app.c
+++ str_app.c
@@ -9,14 +9,14 @@
 
 int rearch(char n)
 {
-if (n < ' ' || n >= '~')
-{
-return (-1);
-}
-else
-{
-return (0);
-}
+	if (n < ' ' || n >= '~')
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 /**
@@ -28,35 +28,35 @@ return (0);
 
 int print_str(va_list arg)
 {
-char *str = va_arg(arg, char *);
-int  i = 0;
-int count = 0;
-while (str[i] != '\0')
-{
-if (rearch(str[i]) != -1)
-{
-_putchar(str[i]);
-count++;
-}
-else
-{
-_putchar('\\');
-_putchar('x');
-count += 2;
-if (str[i] < 16)
-{
-_putchar('0');
-count += _printf("%X", str[i]);
-count++;
-}
-else
-{
-count += _printf("%X", str[i]);
-}
-}
-i++;
-}
-return (count);
+	char *str = va_arg(arg, char *);
+	int  i = 0;
+	int count = 0;
+	while (str[i] != '\0')
+	{
+		if (rearch(str[i]) != -1)
+		{
+			_putchar(str[i]);
+			count++;
+		}
+		else
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			if (str[i] < 16)
+			{
+				_putchar('0');
+				count += _printf("%X", str[i]);
+				count++;
+			}
+			else
+			{
+				count += _printf("%X", str[i]);
+			}
+		}
+		i++;
+	}
+	return (count);
 }
 
 /**
@@ -68,39 +68,39 @@ return (count);
 
 int print_address(void *ptr)
 {
-uintptr_t address = (uintptr_t) ptr;
-char hex_str[sizeof(uintptr_t) * 2 + 3];
-int index = 0, start = 2, end, digit, i = 0, count = 0;
-char temp;
+	uintptr_t address = (uintptr_t) ptr;
+	char hex_str[sizeof(uintptr_t) * 2 + 3];
+	int index = 0, start = 2, end, digit, i = 0, count = 0;
+	char temp;
 
-hex_str[index] = '0';
-index++;
-hex_str[index] = 'x';
-index++;
-while (address > 0)
-{
-digit = address & 0xf;
-hex_str[index] = (digit < 10) ? ('0' + digit) : ('a' + digit - 10);
-index++;
-address >>= 4;
-}
-hex_str[index] = '\0';
-end = index - 1;
-while (start < end)
-{
-temp = hex_str[start];
-hex_str[start] = hex_str[end];
-hex_str[end] = temp;
-start++;
-end--;
-}
-while (hex_str[i] != '\0')
-{
-_putchar(hex_str[i]);
-i++;
-count++;
-}
-return (count);
+	hex_str[index] = '0';
+	index++;
+	hex_str[index] = 'x';
+	index++;
+	while (address > 0)
+	{
+		digit = address & 0xf;
+		hex_str[index] = (digit < 10) ? ('0' + digit) : ('a' + digit - 10);
+		index++;
+		address >>= 4;
+	}
+	hex_str[index] = '\0';
+	end = index - 1;
+	while (start < end)
+	{
+		temp = hex_str[start];
+		hex_str[start] = hex_str[end];
+		hex_str[end] = temp;
+		start++;
+		end--;
+	}
+	while (hex_str[i] != '\0')
+	{
+		_putchar(hex_str[i]);
+		i++;
+		count++;
+	}
+	return (count);
 }
 
 /**
@@ -112,9 +112,9 @@ return (count);
 
 int print_add(va_list arg)
 {
-int n = 0;
-void *p = va_arg(arg, void*);
+	int n = 0;
+	void *p = va_arg(arg, void*);
 
-n += print_address(p);
-return (n);
+	n += print_address(p);
+	return (n);
 }
